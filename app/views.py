2984d08f698a377e2dc203a9e6d3d0abc3e6b8f0
Evgeny <eugen.eisner@gmail.com>
2023-05-04 07:49:13 +0200

    feat: refactor index.html

--- app/views.py
+++ app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from .models import Treatment
 
 
 class IndexView(TemplateView):
     template_name = "app/index.html"
     # form_class = FormContact
     success_url = reverse_lazy("home")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["treatments"] = Treatment.objects.all().exclude(description="")
+        return context
+
     #
     # def form_valid(self, form):
     #     if self.request.POST.get("form_type") == "form_1":
